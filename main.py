6e717afa50f148d20c6ea7cdbb72c3eb800783d6
Kacper <97389155+Siudyy@users.noreply.github.com>
2024-09-13 19:07:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,18 +41,6 @@ async def on_ready():
     print("PAPIEŻO BOT")
     await daily_2137()
 
-async def daily_2137():
-    while True:
-        print("hu")
-        now = datetime.now()
-        then = now.replace(hour=18, minute=43)
-        wait_time = (then-now).total_seconds()
-        
-        print(wait_time)
-        await asyncio.sleep(wait_time)
-        channel = bot.get_channel(1284164179586056232)
-        await channel.send(random.choice(links["storage"]))
-
 if __name__ == "__main__":
     bot.run(token=TOKEN)
 
